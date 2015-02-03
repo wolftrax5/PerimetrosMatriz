017a92b81fbe6df5ac598eab4794f5ff5a24e5ad
Alejandro Medina <5almero5@gmail.com>
2015-02-03 07:49:46 -0600

    Gereracion de matriz resultante, metodo borrra

--- js/Perimetro.js
+++ js/Perimetro.js
@@ -2,6 +2,9 @@ var matrizA;
 var matrizResultante;
 var filasA;
 var colA;
+var contador = 0;
+//contador cuenta las veces que se accede, para que si no es la primera, se borren las capas creadas previamente.
+
 
 function CrearMatriz(n,m){//para Crear la matriz 
 	//Se Define el tamaño de la matriz
@@ -17,9 +20,15 @@ function Generar (){
 	colA = parseInt(document.dimA.elements[1].value);
 	if (isNaN(filasA)|| isNaN(colA) ){
 		alert("Valores no v\u00e1lidos. Son Nulls");
-	}else{
+	}else{ 
+        if (contador > 0) {
+          Borrar()
+        }
 		matrizA= new CrearMatriz(filasA,colA);
 		CrearFormularioMatriz(filasA,colA);
+        matrizResultante = new CrearMatriz(filasA,colA);
+        CrearFormularioResultante(filasA,colA);
+        contador ++;
 	}
 }
 function CrearFormularioMatriz (filasA,colA ){
@@ -35,7 +44,9 @@ function CrearFormularioMatriz (filasA,colA ){
   	var boton = document.createElement("INPUT");
     boton.setAttribute("type", "button");
     boton.setAttribute("value", "CALCULAR");
-// Le mandamos al html la creacion de la tabla por un formulario con casillas para llenar 
+    boton.setAttribute("name" ,"button");
+    boton.onclick= function(){Mostrar();}
+        // Le mandamos al html la creacion de la tabla por un formulario con casillas para llenar 
     for(i=0;i<filasA;i++){
 	var salto = document.createElement("BR");
     	for(j=0;j<colA;j++){
@@ -58,10 +69,86 @@ function CrearFormularioMatriz (filasA,colA ){
     divPrincipal.appendChild(div);
 }
 
+function CrearFormularioResultante(filasA,colA){
+    var div = document.createElement("DIV");
+    var formResultante = document.createElement("FORM");
+    var res = document.createTextNode("Matriz Resultante");
+    div.setAttribute("id","resultado");
+    div.setAttribute("align","center");
+    formResultante.setAttribute("name","matrizR");
+
+    for(i=0;i<filasA;i++){
+        var salto = document.createElement("BR");
+        for(j=0;j<colA;j++){
+            var casilla = document.createElement("INPUT");
+            casilla.setAttribute("type","text");
+            casilla.setAttribute("size","4");
+            casilla.setAttribute("name","text");
+            casilla.readOnly ="true";
+            formResultante.appendChild(casilla);
+        }
+        formResultante.appendChild(salto);
+    }
+    var salto = document.createElement("BR");
+    div.appendChild(salto);
+    div.appendChild(res);
+    div.appendChild(formResultante);
+
+    var divPrincipal=document.getElementById("main");
+    divPrincipal.appendChild(div);
+}
 
 
 
 
+function inicializar(){
+    // se llena la matriz resultante de 0
+    for(i =0 ; i<matrizResultante.length;i++){
+        for(j=0;j<matrizResultante.length;j++){
+            matrizResultante[i][j]=0;
+        }
+    }
+}
+function Evaluar (){
+    inicializar()
+    //Aqui se ara el metodo para lo que se quiere lograr 
+    //aqui solo la igualo 
+    for(i=0;i<filasA;i++){
+        for(j=0;j<colA;j++){
+            matrizResultante[i][j]= matrizA[i][j];
+        }
+    }
+}
+function Cargar(){
+
+    var q =0;
+    for (i=0;i<filasA;i++){
+        for(j=0;j<colA;j++){
+            matrizA[i][j]= parseInt(document.matrizA.elements[q].value);
+            q++;
+        }
+    }
+  Evaluar()
+}
+function Mostrar(){
+
+    Cargar()
+
+     var q =0; 
+    for (var i = 0; i < matrizResultante.length; i++) {
+            for (var j = 0; j < matrizResultante.length; j++) {
+                document.matrizR.elements[q].value = matrizResultante[i][j];
+                matrizResultante[i][j]=0;
+                q++;
+            }
+    }
+}
+
+function Borrar(){ 
+    var mat =  document.getElementById("matrices");
+    var padre1 = mat.parentNode;
+    padre1.removeChild(mat);
+    }
 
 
 
